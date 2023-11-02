e377e322d02d7c507678612cc4c2cb6ee5cf2d76
Iryna Tkachenka <iratsbaton@gmail.com>
2023-11-02 14:46:34 +0100

    First part

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ class PianoRollDisplay {
     }
   }
 
+  selectedPianoRollID = -1;
+
   preparePianoRollCard(rollId) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('piano-roll-card');
@@ -32,6 +34,53 @@ class PianoRollDisplay {
     svg.classList.add('piano-roll-svg');
     svg.setAttribute('width', '80%');
     svg.setAttribute('height', '150');
+    
+    // Make each Piano Roll in the grid clickable
+    cardDiv.addEventListener('click', () => {
+        // If we clicked on the same active tile -> no action
+        if (this.selectedPianoRollID === rollId) {
+          return
+        }
+    
+        this.selectedPianoRollID = rollId;
+
+        const pianoRollContainer = document.getElementById('pianoRollContainer');
+        pianoRollContainer.innerHTML = '';
+
+        // Create blocks for main and rest Piano Rolls
+        const mainPianoRollDiv = document.createElement('div');
+        const restOfPianoRollsDiv = document.createElement('div');
+
+        // Make main Piano Roll block bigger
+        pianoRollContainer.setAttribute('style', 'grid-template-columns: 60% 40%');
+
+        // Add classes to divs of main and rest Piano Rolls blocks
+        mainPianoRollDiv.classList.add('main-div');
+        restOfPianoRollsDiv.classList.add('side-div');
+
+        for (let it = 0; it < 20; it++) {
+          const start = it * 60;
+          const end = start + 60;
+          const partData = this.data.slice(start, end);
+    
+          const { cardDiv, svg } = this.preparePianoRollCard(it)
+
+          // Add to the main Piano Roll block only the selected element
+          if (it !== rollId) {
+            restOfPianoRollsDiv.appendChild(cardDiv);
+          } else {
+            mainPianoRollDiv.appendChild(cardDiv);
+            // Increase hight of main Piano Roll
+            cardDiv.querySelector('svg').setAttribute('height', '300');
+          }
+          
+          const roll = new PianoRoll(svg, partData);
+        }
+
+        pianoRollContainer.appendChild(mainPianoRollDiv);
+        pianoRollContainer.appendChild(restOfPianoRollsDiv);
+      }
+    )
 
     // Append the SVG to the card container
     cardDiv.appendChild(svg);
@@ -45,6 +94,10 @@ class PianoRollDisplay {
     
     const pianoRollContainer = document.getElementById('pianoRollContainer');
     pianoRollContainer.innerHTML = '';
+
+    // Display the Piano Rolls in a responsive grid layout on the main page
+    pianoRollContainer.setAttribute('style', 'grid-template-columns: auto auto auto');
+
     for (let it = 0; it < 20; it++) {
       const start = it * 60;
       const end = start + 60;
